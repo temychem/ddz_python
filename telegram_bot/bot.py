b830f2cdc77b11d152d507263d8609251c97c73a
temychem <emelyanovartem06@mail.ru>
2026-05-17 11:44:14 +0300

    added the function of the work of the tg_bot

---
+++ telegram_bot/bot.py
@@ -0,0 +1,27 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, ConversationHandler, filters, ContextTypes, CallbackContext
+from excel.convert import process_excel
+
+TOKEN = os.getenv("TOKEN")
+
+
+async def handle_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    document = update.message.document
+
+    file = await context.bot.get_file(document.file_id)
+
+    input_path = f"input_{document.file_name}"
+    output_path = f"output_{document.file_name}"
+
+    await file.download_to_drive(input_path)
+
+    process_excel(input_path, output_path)
+
+    await update.message.reply_document(document=open(output_path, "rb"))
+    os.remove(output_path)
+    os.remove(input_path)
+
+
+
+
