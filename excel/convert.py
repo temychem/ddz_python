ae09fdc9ef125f3c584af218392537eff606dbb2
temychem <emelyanovartem06@mail.ru>
2026-06-04 21:01:41 +0300

    Update convert.py
    fixing

--- excel/convert.py
+++ excel/convert.py
@@ -17,54 +17,77 @@ def clean_score(value):
         return None
 
 
-def convert_to_scale(score, max_score):
+def convert_to_scale(
+    score,
+    max_score,
+    grade_5=0.9,
+    grade_4=0.7,
+    grade_3=0.5,
+    grade_2=0.3,
+    grade_1=0.1
+):
+
     if score is None:
         return None
 
     percent = score / max_score
 
-    if percent >= 0.9:
+    if percent >= grade_5:
         return 5
-    elif percent >= 0.7:
+
+    elif percent >= grade_4:
         return 4
-    elif percent >= 0.5:
+
+    elif percent >= grade_3:
         return 3
-    elif percent >= 0.3:
+
+    elif percent >= grade_2:
         return 2
-    elif percent >= 0.1:
+
+    elif percent >= grade_1:
         return 1
-    else:
-        return 0
+
+    return 0
 
 
-def process_excel(file_path, output_path):
+def process_excel(
+    file_path,
+    output_path,
+    grade_5=0.9,
+    grade_4=0.7,
+    grade_3=0.5,
+    grade_2=0.3,
+    grade_1=0.1
+):
+
     df = pd.read_excel(file_path)
 
     for col in df.columns:
 
         if col in ["Fullname", "Номер по списку"]:
             continue
-
+            
         if "(5-балльная)" in col:
             continue
 
         max_score = extract_max_score(col)
+
         if not max_score:
             continue
 
         new_col = f"{col} (5-балльная)"
 
-        if new_col in df.columns:
-            continue
-
         def temp(x):
             return convert_to_scale(x, max_score)
+
+
+        if new_col in df.columns:
+            continue
 
         df[new_col] = (
             df[col]
             .apply(clean_score)
             .apply(temp)
-        )
+            )
 
-        df.to_excel(output_path, index=False)
-
+    df.to_excel(output_path, index=False)
